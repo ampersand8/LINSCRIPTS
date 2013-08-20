cda32ebc19b4b76e53af97dee72f1fc377c7cdaa
ampersand8 <ampersand8@gmail.com>
2013-08-20 11:28:29 +0200

    Update ssh_connect.sh

--- ssh/ssh_connect.sh
+++ ssh/ssh_connect.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 ## Author: Simon Peter, Swisscom IT Services AG
 ## Created: 15.08.2013
-## Last modfied: 16.08.2013
+## Last modfied: 20.08.2013
 ## Version: 0.2
-## Script to sequentially connect to an arbitrary number of ssh servers, in conjunction with auto_connect2.sh
+## Script to sequentially connect to an arbitrary number of ssh servers, in conjunction with auto_connect.sh
 ##### IMPORTANT: REQUIRES auto_connect.sh
+if [[ "`which expect`" != "/usr/bin/expect" ]]
+then
+  echo "Paket expect missing. Please install missing paket."
+  exit 1
+fi
+
 usage() {
   cat<<EOF
   usage: $0 [-h] [-m] [-c command [-e]] server [server...]
@@ -42,12 +48,19 @@ done
 
 echo -n "Passwort: "
 read -s pass
-
+i=0
 for sshserver in $@
 do
     first=$(echo "$sshserver" | cut -c1-1)
     if [[ "$first" != "-" && "$COMMAND" != "$sshserver" ]]
     then
+      ((i++))
       ./auto_connect.sh $sshserver $pass comm=$COMMAND exit=$EXITAFTER master=$MASTER
     fi
 done
+if [[ $i < 1 ]]
+then
+  echo -e "\nPlease enter an sshserver at the end of the command\n"
+  usage
+  exit 1
+fi
