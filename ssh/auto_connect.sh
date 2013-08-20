5e0667b83b5d355002e7a6d81283d39e561e267b
ampersand8 <ampersand8@gmail.com>
2013-08-20 11:29:08 +0200

    Update auto_connect.sh

--- ssh/auto_connect.sh
+++ ssh/auto_connect.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/expect
 ## Author: Simon Peter, Swisscom IT Services AG
 ## Created: 15.08.2013
-## Last modified: 16.08.2013
+## Last modified: 20.08.2013
 ## Version: 0.2
-## Script to automatically connect to SSH Server, in conjunction with ssh_connect2.sh
+## Script to automatically connect to SSH Server, in conjunction with ssh_connect.sh
 
 set timeout 1
 set host [lindex $argv 0]
@@ -11,6 +11,10 @@ set pass [lindex $argv 1]
 set command [lindex [split [lindex $argv 2] "="] 1]
 set exitafter [lindex [split [lindex $argv 3] "="] 1]
 set master [lindex [split [lindex $argv 4] "="] 1]
+
+if {[string length $host] == 0} {
+  puts "auto_connect.sh wird automatisch von ssh_connect.sh aufgerufen. Abbruch..."
+}
 
 set outfile "ssh_connect.log"
 set output [open $outfile a]
